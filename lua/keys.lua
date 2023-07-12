6e53a140b825773a0ac83a0e30a4fc05ebbcbeaa
Ryan Rawlings <rjrawlings@gmail.com>
2023-07-12 09:23:37 -0600

    use leader key for toggling nvim tree

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,7 +1,12 @@
 local map = vim.api.nvim_set_keymap
 
 local api = require "nvim-tree.api"
-map('n', 'n', '', {callback = function()
+map('n', '<Leader>n', '', {callback = function()
   api.tree.toggle({})
+end
+})
+
+map('n', '<Leader>m', '', {callback = function()
+  api.tree.focus({})
 end
 })
