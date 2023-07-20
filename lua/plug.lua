73ca936634875960073ef9dc85ac8387ea6fcbff
Ryan Rawlings <rjrawlings@gmail.com>
2023-07-19 22:49:32 -0600

    wrap use blocks in curly braces, for consistency.

--- lua/plug.lua
+++ lua/plug.lua
@@ -12,27 +12,28 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use {'nvim-tree/nvim-tree.lua', requires = 'nvim-tree/nvim-web-devicons'}
+    use {'wbthomason/packer.nvim'}
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = 'nvim-tree/nvim-web-devicons'
+    }
 
-    -- [[ Theme ]]
-    use 'mhinz/vim-startify' 
-    use 'nvim-lualine/lualine.nvim'
-    use 'Mofiqul/dracula.nvim' 
+    use {'mhinz/vim-startify'}
+    use {'Mofiqul/dracula.nvim'}
+    use {'nvim-lualine/lualine.nvim'}
 
-      -- [[ Dev ]]
-    use 'numToStr/Comment.nvim'
+    use {'numToStr/Comment.nvim'}
     use {
         'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = 'nvim-lua/plenary.nvim'
     }
     use {"akinsho/toggleterm.nvim", tag = 'v2.7.1'}
-    use 'majutsushi/tagbar'
-    use 'lukas-reineke/indent-blankline.nvim'
-    use 'tpope/vim-fugitive'
-    use 'junegunn/gv.vim'
-    use 'windwp/nvim-autopairs'
-    use 'lewis6991/gitsigns.nvim'
+    use {'majutsushi/tagbar'}
+    use {'lukas-reineke/indent-blankline.nvim'}
+    use {'tpope/vim-fugitive'}
+    use {'junegunn/gv.vim'}
+    use {'windwp/nvim-autopairs'}
+    use {'lewis6991/gitsigns.nvim'}
     -- use {'akinsho/bufferline.nvim', tag = "v4.3.0", requires = 'nvim-tree/nvim-web-devicons'}
 
     if packer_bootstrap then
