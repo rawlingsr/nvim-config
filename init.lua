728347bbbfe5e36582f16929b5a66afb01041be0
Ryan Rawlings <rjrawlings@gmail.com>
2023-07-21 23:21:39 -0600

    use powershell

--- init.lua
+++ init.lua
@@ -1,4 +1,20 @@
---Rip this out when nvim tree goes
+local shell_options = {}
+if vim.fn.executable "pwsh" == 1 then
+    shell_options = {
+        shell = "pwsh",
+        shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+        shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+        shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+        shellquote = "",
+        shellxquote = "",
+    }
+end
+
+for option, value in pairs(shell_options) do
+    vim.opt[option] = value
+end
+
+-- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
