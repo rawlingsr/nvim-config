3e3799cda4769a440a847c1501fb97e9cbe7a678
Ryan Rawlings <rjrawlings@gmail.com>
2023-08-26 00:17:28 -0600

    delete mostly unused vars file

--- init.lua
+++ init.lua
@@ -25,8 +25,11 @@ vim.g.mapleader = ','
 -- Commenting out for now, need to find a better binding before making better use of local leader.
 -- vim.g.localleader = '\\'
 
+-- set a very basic theme in case a good theme is not available.
+vim.g.t_co = 256
+vim.g.background = 'dark'
+
 -- Imports
-require('vars')
 require('opts')
 require('plug')
 require('keys')
