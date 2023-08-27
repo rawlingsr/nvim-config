b7b81efa4dc58bd5250e11614dad7277724e4e8c
Ryan Rawlings <rjrawlings@gmail.com>
2023-08-26 23:10:20 -0600

    clean up keymap a bit

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,13 +1,14 @@
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 
+-- I couldn't find a lua way to open neotree
 map('n', '<leader>/', ":Neotree<CR>", {desc = "open neotree"})
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+map('n', '<leader>e', vim.diagnostic.open_float)
+map('n', '[d', vim.diagnostic.goto_prev)
+map('n', ']d', vim.diagnostic.goto_next)
+map('n', '<leader>q', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -27,21 +28,21 @@ vim.api.nvim_create_autocmd('LspAttach', {
         return opts
     end
 
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, desc("Go to Declaration"))
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, desc("Go to Definition"))
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, desc("Hover"))
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, desc("Go to Implementation"))
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, desc("Signature help"))
-    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, desc("Add workspace folder"))
-    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, desc("Remove workspace folder"))
-    vim.keymap.set('n', '<leader>wl', function()
+    map('n', 'gD', vim.lsp.buf.declaration, desc("Go to Declaration"))
+    map('n', 'gd', vim.lsp.buf.definition, desc("Go to Definition"))
+    map('n', 'K', vim.lsp.buf.hover, desc("Hover"))
+    map('n', 'gi', vim.lsp.buf.implementation, desc("Go to Implementation"))
+    map('n', '<C-k>', vim.lsp.buf.signature_help, desc("Signature help"))
+    map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, desc("Add workspace folder"))
+    map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, desc("Remove workspace folder"))
+    map('n', '<leader>wl', function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, desc("List workspace folders"))
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, desc("Show Type Definition"))
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, desc("LS rename"))
-    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, desc("Code actions"))
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, desc("Get References?"))
-    vim.keymap.set('n', '<leader>f', function()
+    map('n', '<leader>D', vim.lsp.buf.type_definition, desc("Show Type Definition"))
+    map('n', '<leader>rn', vim.lsp.buf.rename, desc("LS rename"))
+    map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, desc("Code actions"))
+    map('n', 'gr', vim.lsp.buf.references, desc("Get References?"))
+    map('n', '<leader>f', function()
       vim.lsp.buf.format { async = true }
     end, desc("Format Document"))
   end,
