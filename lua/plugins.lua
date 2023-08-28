511db82b3c94046fd9208e8f5d3f50d636f65463
Ryan Rawlings <rjrawlings@gmail.com>
2023-08-28 12:10:09 -0600

    Migrate more plugins from packer to lazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
@@ -9,17 +10,67 @@ if not vim.loop.fs_stat(lazypath) then
         lazypath,
     })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 plugins = {
+    -- THEME
+    "nvim-tree/nvim-web-devicons",
+    {
+        'Mofiqul/dracula.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.api.nvim_command('colorscheme dracula')
+        end,
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        opts = { theme = 'dracula-nvim' },
+    },
+    {
+        'lukas-reineke/indent-blankline.nvim',
+            opts = {
+                show_end_of_line = true,
+                space_char_blankline = " ",
+            },
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        }
+    },
+
+
+    -- GIT
     'tpope/vim-fugitive',
     'junegunn/gv.vim',
+
+    -- LIBS
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+
+    -- CONVENIENCE
     'mhinz/vim-startify',
     {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
         opts = {}
+    },
+    {
+        'numToStr/Comment.nvim',
+        opts = {},
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = 'nvim-lua/plenary.nvim',
     }
+
+
 }
 
 require("lazy").setup(plugins)
