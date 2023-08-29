0c18dcfbe2c76212cb921989699044b12d062629
Ryan Rawlings <rjrawlings@gmail.com>
2023-08-28 23:35:02 -0600

    add telescope fuzzy finder optional dep

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -56,7 +56,10 @@ local plugins = {
     -- LIBS
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    },
     -- CONVENIENCE
     'mhinz/vim-startify',
     {
