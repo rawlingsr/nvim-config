ed7896e180f9b049b85bf0bc3e688ec6c077437a
Ryan Rawlings <rjrawlings@gmail.com>
2023-08-29 10:40:23 -0600

    add mappings in neotree window

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,6 +27,15 @@ local M = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
             "MunifTanjim/nui.nvim",
+        },
+        opts = {
+            window = {
+                mappings = {
+                    ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
+                    ['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
+                    ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
+                },
+  },
         }
     },
 
