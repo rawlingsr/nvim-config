dd8e2af68efc67891123fad9cafee463b3febc1c
Ryan Rawlings <rjrawlings@gmail.com>
2023-08-29 17:09:22 -0600

    show sign column always

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,7 @@ opt.colorcolumn = '120' -- Highlight nth column
 opt.number = true
 opt.relativenumber = true
 opt.scrolloff = 4 -- How close curser can get to the edge
+opt.signcolumn = 'yes'
 
 -- [[ Theme ]]
 opt.guifont = 'Source Code Pro Nerd Font:12, Fira Code:12, Source Code Pro:12, Consolas:12'
